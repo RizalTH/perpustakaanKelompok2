749bb35b51da1e7e02b36d5e295cc08cafea1ef3
Firman Adiyuda <firmanadiyuda@gmail.com>
2019-12-31 23:32:13 +0700

    Initialize for IntelliJ IDEA

--- Perpusoft/src/perpusoft/MainClass.java
+++ Perpusoft/src/perpusoft/MainClass.java
@@ -1,9 +1,15 @@
 package perpusoft;
+import javax.swing.JFrame;
 
-public class MainClass {
+public class MainClass extends JFrame {
 
+    public MainClass(){
+        this.setTitle("TESSSS");
+        this.setSize(600,320);
+    }
     public static void main(String[] args) {
-
+        MainClass mWindow = new MainClass();
+        mWindow.setVisible(true);
     }
     
 }
